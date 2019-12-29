c3918e88493c6e7e4f0f3c7ba5de222b5847548c
MoonManModding <moonmanmodding@gmail.com>
2019-12-29 00:43:44 +0000

    Balance

--- src/main/java/io/moonman/emergingtechnology/config/synthetics/SyntheticsModuleBioreactor.java
+++ src/main/java/io/moonman/emergingtechnology/config/synthetics/SyntheticsModuleBioreactor.java
@@ -8,17 +8,17 @@ public class SyntheticsModuleBioreactor {
 
     @Name("Tissue Bioreactor Energy Usage")
     @Config.Comment("How much energy the Tissue Bioreactor uses per cycle (~10 ticks).")
-    @RangeInt(min = 0, max = 100)
-    public int bioreactorEnergyUsage = 1;
+    @RangeInt(min = 0, max = 1000)
+    public int bioreactorEnergyUsage = 100;
     
     @Name("Tissue Bioreactor Water Usage")
     @Config.Comment("How much water the Tissue Bioreactor uses per cycle (~10 ticks).")
-    @RangeInt(min = 0, max = 100)
-    public int bioreactorWaterUsage = 1;
+    @RangeInt(min = 0, max = 1000)
+    public int bioreactorWaterUsage = 100;
 
     @Name("Tissue Bioreactor Operation Time")
     @Config.Comment("How long the Tissue Bioreactor takes to create samples from syringes.")
-    @RangeInt(min = 0, max = 100)
-    public int bioreactorBaseTimeTaken = 50;
+    @RangeInt(min = 0, max = 1000)
+    public int bioreactorBaseTimeTaken = 200;
 
 }
--- src/main/java/io/moonman/emergingtechnology/config/synthetics/SyntheticsModuleScaffolder.java
+++ src/main/java/io/moonman/emergingtechnology/config/synthetics/SyntheticsModuleScaffolder.java
@@ -9,10 +9,10 @@ public class SyntheticsModuleScaffolder {
     @Name("Tissue Scaffolder Energy Usage")
     @Config.Comment("How much energy the Tissue Scaffolder uses per cycle (~10 ticks).")
     @RangeInt(min = 0, max = 100)
-    public int scaffolderEnergyUsage = 1;
+    public int scaffolderEnergyUsage = 75;
 
     @Name("Tissue Scaffolder Operation Time")
     @Config.Comment("How long the Tissue Scaffolder takes to create samples from syringes.")
-    @RangeInt(min = 0, max = 100)
-    public int scaffolderBaseTimeTaken = 50;
+    @RangeInt(min = 0, max = 1000)
+    public int scaffolderBaseTimeTaken = 150;
 }
