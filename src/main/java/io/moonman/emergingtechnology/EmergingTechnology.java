fd859c43e2ef42eac1ea91caa181a0e791785679
MoonManModding <moonmanmodding@gmail.com>
2020-01-05 08:57:41 +0000

    Pre 1.2.14

--- src/main/java/io/moonman/emergingtechnology/EmergingTechnology.java
+++ src/main/java/io/moonman/emergingtechnology/EmergingTechnology.java
@@ -16,7 +16,7 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
 public class EmergingTechnology {
   public static final String MODID = "emergingtechnology";
   public static final String NAME = "Emerging Technology";
-  public static final String VERSION = "1.2.13-beta";
+  public static final String VERSION = "1.2.14-beta";
 
   public static final TechnologyTab TECHNOLOGYTAB = new TechnologyTab("technologytab");
 
--- src/main/java/io/moonman/emergingtechnology/proxy/ClientOnlyProxy.java
+++ src/main/java/io/moonman/emergingtechnology/proxy/ClientOnlyProxy.java
@@ -2,18 +2,15 @@ package io.moonman.emergingtechnology.proxy;
 
 import io.moonman.emergingtechnology.EmergingTechnology;
 import io.moonman.emergingtechnology.config.EmergingTechnologyConfig;
-import io.moonman.emergingtechnology.init.RegistrationHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.audio.SoundHandler;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.obj.OBJLoader;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ClientOnlyProxy extends CommonProxy {
 
@@ -31,11 +28,6 @@ public class ClientOnlyProxy extends CommonProxy {
 		super.init(event);
 		
 		MinecraftForge.EVENT_BUS.register(SoundHandler.class);
-    }
-
-    @SubscribeEvent
-    public static void registerModels(ModelRegistryEvent event) {
-        RegistrationHandler.registerModels(event);
     }
     
     @Override
