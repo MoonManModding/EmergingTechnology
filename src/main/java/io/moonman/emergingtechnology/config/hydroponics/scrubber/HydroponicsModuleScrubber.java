bc30a5eaabcc439c79215b90011dca748efbc44e
MoonMan <moonmanmodding@gmail.com>
2020-02-04 23:42:52 +0000

    Update HydroponicsModuleScrubber.java

--- src/main/java/io/moonman/emergingtechnology/config/hydroponics/scrubber/HydroponicsModuleScrubber.java
+++ src/main/java/io/moonman/emergingtechnology/config/hydroponics/scrubber/HydroponicsModuleScrubber.java
@@ -24,7 +24,7 @@ public class HydroponicsModuleScrubber {
     @Name("Scrubber - CO2 capture rate")
     @Config.Comment("The amount of CO2 captured per cycle (~10 ticks).")
     @RangeInt(min = 1, max = 1000)
-    public int scrubberGasGenerated = 100;
+    public int scrubberGasGenerated = 5;
 
     @Name("Scrubber - Operation Time")
     @Config.Comment("The time taken for the Scrubber to process Carbon Dioxide")
