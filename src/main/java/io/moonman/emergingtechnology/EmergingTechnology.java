7339a5753772c781448769112863818621ec3a60
MoonMan <moonmanmodding@gmail.com>
2020-03-03 15:27:09 +0000

    1.14 version

--- src/main/java/io/moonman/emergingtechnology/EmergingTechnology.java
+++ src/main/java/io/moonman/emergingtechnology/EmergingTechnology.java
@@ -17,7 +17,7 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
 public class EmergingTechnology {
   public static final String MODID = "emergingtechnology";
   public static final String NAME = "Emerging Technology";
-  public static final String VERSION = "TEST";
+  public static final String VERSION = "0.0.0-alpha";
 
   public static final TechnologyTab TECHNOLOGYTAB = new TechnologyTab("technologytab");
 
